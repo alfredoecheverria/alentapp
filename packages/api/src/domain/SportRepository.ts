28cd07e7e34f5722df5f47b560581d07a05513df
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-17 12:30:34 -0300

    feat(update-sport): Define e implementa findById en el repositorio y postgres

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -3,6 +3,7 @@ import { SportDTO, UpdateSportRequest } from '@alentapp/shared';
 export interface SportRepository {
     create(sport: Omit<SportDTO, 'id'>): Promise<SportDTO>;
     findByName(name: string): Promise<SportDTO | null>;
+    findById(id: string): Promise<SportDTO | null>;
     findAll(): Promise<SportDTO[]>;
     update(id: string, data: UpdateSportRequest): Promise<SportDTO>;
 }
