9178303f260631a0f4458bb1e892e13bcd4a98dc
Avril Lugo Gonzalez <avriluchigonzalez@gmail.com>
2026-05-17 13:01:53 -0300

    feat(create-payment): implementa caso de uso para crear un pago nuevo

---
+++ packages/api/src/application/CreatePaymentUseCase.ts
@@ -0,0 +1,25 @@
+import { PaymentRepository } from "../domain/PaymentRepository.js";
+import { PaymentDTO, CreatePaymentRequest } from "@alentapp/shared/index.js";
+import { PaymentValidator } from '../domain/services/PaymentValidator.js';
+
+export class CreatePaymentUseCase {
+    constructor(
+        private readonly paymentRepository: PaymentRepository,
+        private readonly paymentValidator: PaymentValidator 
+    ) {}
+    async execute(data: CreatePaymentRequest):Promise<PaymentDTO> {
+
+        this.paymentValidator.validateAmount(data.amount);
+        this.paymentValidator.validateDateFormat(data.due_date)
+        this.paymentValidator.validateDateFormat(data.payment_date)
+        this.paymentValidator.validateInitialStatus(data.status)
+        this.paymentValidator.validateYearRange(data.year)
+        this.paymentValidator.validateMonthRange(data.month)
+
+        const nuevoPago = await this.paymentRepository.create({
+            ...data
+        });
+
+        return nuevoPago;
+    }
+}
