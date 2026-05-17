8e79d5dbfd823e5471c4e9226641c6c0a052b7d5
ignacio williams <ignacio.williams@alu.frlp.utn.edu.ar>
2026-05-17 09:37:54 -0300

    feat(create-equipment-loan): Implementacion del EquipmentLoanValidator, EquipmentLoanRepository y EquipmentLoanUseCase

---
+++ packages/api/src/domain/services/EquipmentLoanValidator.ts
@@ -0,0 +1,38 @@
+import { EquipmentLoanRepository } from '../EquipmentLoanRepository.js';
+import { MemberRepository } from '../MemberRepository.js';
+
+export class EquipmentLoanValidator {
+    constructor(
+        private readonly equipmentLoanRepo: EquipmentLoanRepository,
+        private readonly memberRepo: MemberRepository
+    ) {}
+
+
+    async validateLoanMemberExists(memberId: string): Promise<void> {
+        
+        const memberExists = await this.memberRepo.findById(memberId);
+        
+        if (!memberExists) {
+            throw new Error('El usuario no existe');
+        }
+    }
+
+
+    async validateMemberCategoryForLoan(memberId: string): Promise<void> {
+        const member = await this.memberRepo.findById(memberId);
+        
+        if (member && member.category === 'Cadete') {
+            throw new Error('Solo se permite realizar prestamos a miembros con categoria Senior o Lifetime');
+        }
+    }
+
+
+    //Fecha de prestamo posterior a fecha de devolucion
+    validateLoanDates(loanDate: string, dueDate: string): void {
+        const loanDateObj = new Date(loanDate);
+        const dueDateObj = new Date(dueDate);
+        if (loanDateObj >= dueDateObj) {
+            throw new Error('Fecha prestamo no puede ser posterior a Fecha Devolucion');
+        }
+    }
+}
