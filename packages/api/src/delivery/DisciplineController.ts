310e05915e9744924daea9da1d761031cda2b999
Alfredo Echeverria <alfredoecheverriapalma@gmail.com>
2026-05-17 17:50:35 -0300

    feat(get-all-disciplines): agrega endpoint GET para listar sanciones

--- packages/api/src/delivery/DisciplineController.ts
+++ packages/api/src/delivery/DisciplineController.ts
@@ -1,12 +1,23 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { CreateDisciplineUseCase } from '../application/CreateDisciplineUseCase.js';
 import { CreateDisciplineRequest } from '@alentapp/shared';
+import { GetDisciplinesUseCase } from '../application/GetDisciplinesUseCase.js';
 
 export class DisciplineController {
     constructor(
         private readonly createDisciplineUseCase: CreateDisciplineUseCase,
+        private readonly getDisciplinesUseCase: GetDisciplinesUseCase,
     ) {}
 
+    async getAll(_request: FastifyRequest, reply: FastifyReply) {
+        try {
+            const disciplinas = await this.getDisciplinesUseCase.execute();
+            return reply.status(200).send({ data: disciplinas });
+        } catch (error: any) {
+            return reply.status(500).send({ error: error.message });
+        }
+    } 
+       
     async create(
         request: FastifyRequest<{ Body: CreateDisciplineRequest }>,
         reply: FastifyReply,
