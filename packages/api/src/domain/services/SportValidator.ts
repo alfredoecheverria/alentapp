393fd6619f130a5a819b17a8f932d6e965be1b3d
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-16 21:54:26 -0300

    feat(create-sport): Implementa SportValidator para centralizar validaciones

---
+++ packages/api/src/domain/services/SportValidator.ts
@@ -0,0 +1,24 @@
+import { SportRepository } from "../SportRepository.ts";
+
+export class SportValidator {
+    constructor(private readonly sportRepo: SportRepository) {}
+
+    validateMaxCapacity(max_capacity: number): void {
+       if(max_capacity <= 0) {
+            throw new Error('Capacidad máxima inválida');
+       }
+    }
+
+    validateAdditionalPrice(additional_price: float): void {
+        if(additional_price < 0) {
+            throw new Error('El valor de precio adicional debe ser un numero igual o mayor a 0');
+        }
+    }
+
+    async validateNameIsUnique(name: string): Promise<void> {
+        const exists = await this.sportRepo.findByName(name);
+        if (exists) {
+            throw new Error('Ya existe un deporte con ese nombre');
+        }
+    }
+}
