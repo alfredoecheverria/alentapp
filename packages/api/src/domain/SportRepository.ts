672da3df5f8670c501881b4a8b2fc715383aa1c8
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-17 12:21:57 -0300

    feat(update-sport): Agrega puerto de entratda para modificacion de deporte

--- packages/api/src/domain/SportRepository.ts
+++ packages/api/src/domain/SportRepository.ts
@@ -1,7 +1,8 @@
-import { SportDTO } from '@alentapp/shared';
+import { SportDTO, UpdateSportRequest } from '@alentapp/shared';
 
 export interface SportRepository {
     create(sport: Omit<SportDTO, 'id'>): Promise<SportDTO>;
     findByName(name: string): Promise<SportDTO | null>;
     findAll(): Promise<SportDTO[]>;
+    update(id: string, data: UpdateSportRequest): Promise<SportDTO>;
 }
