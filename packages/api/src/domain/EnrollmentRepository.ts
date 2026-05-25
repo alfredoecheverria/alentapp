f789699bd00e54f799eb5d2ae9ff08a8e671b0f8
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-25 15:06:49 -0300

    feat(update-enrollment): Agrega puerto de entrada para modificacion de inscripcion

--- packages/api/src/domain/EnrollmentRepository.ts
+++ packages/api/src/domain/EnrollmentRepository.ts
@@ -1,8 +1,10 @@
-import { EnrollmentDTO } from '@alentapp/shared';
+import { EnrollmentDTO, UpdateEnrollmentRequest } from '@alentapp/shared';
 
 export interface EnrollmentRepository {
     create(enrollment: Omit<EnrollmentDTO, 'id'>): Promise<EnrollmentDTO>;
     findByMemberIdAndSportId(member_id: string, sport_id: string): Promise<EnrollmentDTO | null>;
+    findById(id: string): Promise<EnrollmentDTO | null>;
     findAllBySportId(id: string): Promise<EnrollmentDTO[]>;
     findAll(): Promise<EnrollmentDTO[]>;
+    update(id: string, data: UpdateEnrollmentRequest): Promise<EnrollmentDTO>;
 }
