5ae9e974397cdc85659896bf7970fccb6673be61
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-25 15:09:07 -0300

    feat(update-enrollment): Implementa Caso de Uso modificar inscripcion

---
+++ packages/api/src/application/UpdateEnrollmentUseCase.ts
@@ -0,0 +1,26 @@
+import { EnrollmentRepository } from "../domain/EnrollmentRepository.ts";
+import { EnrollmentValidator } from "../domain/services/EnrollmentValidator.ts";
+import { EnrollmentDTO, UpdateEnrollmentRequest } from "@alentapp/shared";
+
+export class UpdateEnrollmentUseCase {
+    constructor (
+        private readonly enrollmentRepository: EnrollmentRepository,
+        private readonly enrollmentValidator: EnrollmentValidator,
+    ) {}
+
+    async execute(id: string, data: UpdateEnrollmentRequest): Promise<EnrollmentDTO> {
+        // Valida que haya datos a modificar en el pedido
+        this.enrollmentValidator.validateEmptyRequest(data);
+
+        // Valida que exista la inscripcion
+        await this.enrollmentValidator.validateEnrollmentExists(id);
+
+        // Valida que no se intente modificar el socio o deporte
+        this.enrollmentValidator.validateNoMemberId(data);
+        this.enrollmentValidator.validateNoSportId(data);
+
+        //Me aseguro que solo se puedan actualizar los valores enrollment_date y/o is_active
+        let finalData = {enrollment_date: data.enrollment_date, is_active: data.is_active};
+        await this.enrollmentRepository.update(id, finalData);
+    }
+}
