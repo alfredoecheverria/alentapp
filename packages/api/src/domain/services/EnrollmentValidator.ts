23808fb7f32926a0d5f02ff1628753decdceb567
Juan Ignacio Piazza <azzaipjuan@gmail.com>
2026-05-25 15:08:14 -0300

    feat(update-enrollment): Implementa validaciones requeridas para el caso de uso update

--- packages/api/src/domain/services/EnrollmentValidator.ts
+++ packages/api/src/domain/services/EnrollmentValidator.ts
@@ -7,6 +7,31 @@ export class EnrollmentValidator {
         private readonly sportRepo: SportRepository
     ) {}
 
+    validateEmptyRequest(data: Object) {
+        if (data === null || data === undefined || Object.keys(data).length === 0) {
+            throw new Error('Debe indicar al menos un campo a modificar');
+        }
+    }
+
+    validateNoMemberId(data: Object) {
+        if('member_id' in data) {
+            throw new Error('No se puede editar el socio asociado');
+        }
+    }
+
+    validateNoSportId(data: Object) {
+        if('sport_id' in data) {
+            throw new Error('No se puede editar el deporte asociado');
+        }
+    }
+
+    async validateEnrollmentExists(id: string) {
+        const exists = await this.enrollmentRepo.findById(id);
+        if (!exists) {
+            throw new Error('La inscripción no existe');
+        }
+    }
+
     async validateMemberIsNotEnrolledYet(member_id: string, sport_id: string) {
         const exists = await this.enrollmentRepo.findByMemberIdAndSportId(member_id, sport_id);
         if (exists) {
